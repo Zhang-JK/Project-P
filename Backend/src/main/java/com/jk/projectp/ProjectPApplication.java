35cd54bb255f5653b5f763b37cad3fc26a4db44d
Zhang Jiekai <zjkisgood@126.com>
2022-06-15 17:37:33 +0800

    init program

---
+++ Backend/src/main/java/com/jk/projectp/ProjectPApplication.java
@@ -0,0 +1,13 @@
+package com.jk.projectp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/jk/projectp/ProjectPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.projectp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
