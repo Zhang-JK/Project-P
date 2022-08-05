debcbec7d70953285ba3b4b81e46cf80edbba5aa
ZENG-Zhuo <zzengan@conncet.ust.hk>
2022-08-05 20:27:34 +0800

    change the login part to js

---
+++ frontend/src/Components/Login/index.js
@@ -0,0 +1,53 @@
+import {config} from "../../Config";
+import React from "react";
+import {md5} from "hash-wasm";
+
+
+const Login = () => {
+    const usernameI = React.createRef();
+    const passwordI = React.createRef();
+    let encodedPassword;
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <label>
+                    Username:
+                    <input ref={usernameI} type="text"/>
+                </label>
+                <label>
+                    Password:
+                    <input ref={passwordI} type="password"/>
+                </label>
+                <button type="button" onClick={async () => {
+                    console.log(111);
+                    await md5(passwordI.current.value).then(res => {
+                        encodedPassword = res;
+                        console.log(res);
+                    });
+                    const requestOptions = {
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({
+                            'username': usernameI.current.value.toString(),
+                        'password': encodedPassword
+                })
+                };
+                    console.log(" sending request1");
+                    fetch(config.apiUrl + "login", requestOptions).then(response => response.json()).then(data => {
+                        const {code, msg} = data;
+                        console.log("raw data: " + JSON.stringify(data));
+                        if (code === 200) {
+                            alert(" login success");
+                            // navigate('/index', {replace: false});
+                        } else {
+                            alert("Error: " + msg);
+                        }
+                    })
+                }}>Login
+                </button>
+            </form>
+        </div>
+    );
+}
+export default Login;
---
+++ frontend/src/Pages/LoginPage/index.js
@@ -0,0 +1,12 @@
+import Login from "../../Components/Login";
+
+
+const LoginPage = ()=>{
+    return (
+        <div>
+            <Login/>
+        </div>
+    );
+}
+
+export default LoginPage;
